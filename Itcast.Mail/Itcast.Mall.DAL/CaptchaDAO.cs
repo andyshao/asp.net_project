9e7a716096be32ef991ffaf6fccbe288a250433f
JackyCodee <81243232@qq.com>
2015-09-28 23:18:49 +0800

    1 day
    7日计划

---
+++ Itcast.Mail/Itcast.Mall.DAL/CaptchaDAO.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Itcast.Mall.Model;
+using System.Data.SqlClient;
+using Itcast.Mall.Utility;
+
+
+namespace Itcast.Mall.DAL
+{
+    public class CaptchaDAO
+    {
+        public IEnumerable<Captcha> GetList() {
+            #region MyRegion  
+            //    using (SqlConnection conn = new SqlConnection(""))
+            //    {
+            //        using (SqlCommand cmd = conn.CreateCommand())
+            //        {
+            //            cmd.CommandText = "select * from Captcha";
+            //                conn.Open();
+            //                using (SqlDataReader reader = cmd.ExecuteReader())
+            //                {
+            //                    if (reader.HasRows)
+            //                    {
+            //                        IList<Captcha> list = new List<Captcha>();
+            //                        while (reader.Read())
+            //                        {
+            //                            Captcha model = new Captcha
+            //                            {
+            //                                Id = Convert.ToInt32(reader.GetValue(0)),
+            //                                UserId = Convert.ToInt32(reader.GetValue(1)),
+            //                                Token = Convert.ToString(reader.GetValue(2)),
+            //                                Actived = Convert.ToBoolean(reader.GetValue(3)),
+            //                                Expired = Convert.ToDateTime(reader.GetValue(4))
+            //                            };
+            //                            list.Add(model);
+            //                        }
+            //                        return list;
+            //                    }
+            //                }
+            //          }
+            //    }
+            //    return null; 
+            #endregion
+
+            #region MyRegion
+            //using(var reader = SqlHelper.ExecuteReader(""))
+            //{
+            //    if(reader.HasRows)
+            //    {
+            //        IList<Captcha> list = new List<Captcha>();
+            //        while (reader.Read())
+            //        {
+            //            list.Add(SqlHelper.MapEntity<Captcha>(reader));
+            //        }
+            //        return list;
+            //    }
+            //}
+            //return null; 
+            #endregion
+            using (var reader = SqlHelper.ExecuteReader(""))
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        yield return SqlHelper.MapEntity<Captcha>(reader);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Itcast.Mail/Itcast.Mall.Model/Captcha.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Itcast.Mall.Model
+{
+    public class Captcha
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Token { get; set; }
+        public bool Actived { get; set; }
+        public DateTime Expired { get; set; }
+    }
+}
